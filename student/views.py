908c1ffbe743f6b82e6a7a75d4de32e5b51e9918
nqttruong <truongcau2003@gmail.com>
2025-09-18 02:04:42 +0700

    edit_delete

--- student/views.py
+++ student/views.py
@@ -1,6 +1,7 @@
-from pyexpat.errors import messages
 
-from django.shortcuts import render, get_object_or_404
+from pyexpat.errors import messages
+from http.client import HTTPResponse, HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 # Create your views here.
 def list_student(request):
@@ -76,8 +77,63 @@ def add_student(request):
 
     return render(request, "students/add-student.html")
 
-def edit_student(request):
+def edit_student(request, slug):
+    student = get_object_or_404(Student, slug=slug)
+    parent = student.parent if hasattr(student, 'parent') else None
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        student_id = request.POST.get("student_id")
+        gender = request.POST.get("gender")
+        date_of_birth = request.POST.get("date_of_birth")
+        student_class = request.POST.get("student_class")
+        religion = request.POST.get('religion')
+        joining_date = request.POST.get("joining_date")
+        mobile_number = request.POST.get("mobile_number")
+        admission_number = request.POST.get('admission_number')
+        section = request.POST.get('section')
+        student_image = request.FILES.get('student_image') if request.FILES.get('student_image') else student.student_image
+
+        parent.father_name = request.POST.get('father_name')
+        parent.father_occupation = request.POST.get('father_occupation')
+        parent.father_mobile = request.POST.get('father_mobile')
+        parent.father_email = request.POST.get('father_email')
+        parent.mother_name = request.POST.get('mother_name')
+        parent.mother_occupation = request.POST.get('mother_occupation')
+        parent.mother_mobile = request.POST.get('mother_mobile')
+        parent.mother_email = request.POST.get('mother_email')
+        parent.present_address = request.POST.get('present_address')
+        parent.permanent_address = request.POST.get('permanent_address')
+        parent.save()
+
+        student.first_name= first_name
+        student.last_name= last_name
+        student.student_id= student_id
+        student.gender= gender
+        student.date_of_birth= date_of_birth
+        student.student_class= student_class
+        student.religion= religion
+        student.joining_date= joining_date
+        student.mobile_number = mobile_number
+        student.admission_number = admission_number
+        student.section = section
+        student.student_image = student_image
+        student.save()
+        # context = {
+        #     'student': student,
+        #     'parent': parent
+        # }
+
+        # create_notification(request.user, f"Added Student: {student.first_name} {student.last_name}")
+
+        return redirect("student_list")
     return render(request, "students/edit-student.html")
 
-def detail(request):
-    return render(request, "students/student-details.html")
+    return render(request, "students/edit-student.html")
+
+def delete_student(request, slug):
+    if request.method == "POST":
+        student = get_object_or_404(Student, slug=slug)
+        student.delete()
+        return redirect ('student_list')
+    return HttpResponseForbidden()
