29525280182f953fe340ef537b1eb8de8cfd91b3
nqttruong <truongcau2003@gmail.com>
2025-09-22 23:47:28 +0700

    home_auth

--- home_auth/models.py
+++ home_auth/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from typing_extensions import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add = True)
+    is_authorized = models.BooleanField(default=False)
+
+    is_student = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name = True,
+        blank = True
+    )
