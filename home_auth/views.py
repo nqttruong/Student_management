d5e1b14678ed5a9b6c122f24b073e694f489398a
nqttruong <truongcau2003@gmail.com>
2025-09-25 23:24:03 +0700

    add_view_signup

--- home_auth/views.py
+++ home_auth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        role = request.POST['role']
+
+
